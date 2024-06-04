586b0b1d940650b8179f3096216aa1bcacf98713
Кестнер Дмитрий <kdd@agsr.loc>
2024-06-04 15:07:22 +0300

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: ['airbnb', 'airbnb/hooks', 'airbnb-typescript', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:prettier/recommended'],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react-refresh', 'react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react/react-in-jsx-scope': 0,
+    'import/no-extraneous-dependencies': 'off',
+    'jsx-quotes': ['error', 'prefer-single'],
+    'max-len': [
+      2,
+      180,
+      {
+        ignorePattern: '^(import|export)\\s.+\\sfrom\\s.+;$',
+        ignoreUrls: true,
+        ignoreComments: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+      },
+    ],
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useEffect } from 'react';
+
+function App() {
+  useEffect(() => {}, []);
+
+  return <div>Hello</div>;
+}
+
+export default App;
